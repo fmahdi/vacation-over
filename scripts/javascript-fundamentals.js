a8206a10b974807d6305caeb406903fda24f5791
Fahim Mahdi <fmb.mondal@gmail.com>
2024-05-24 01:05:58 +0600

    added JS files

---
+++ scripts/javascript-fundamentals.js
@@ -0,0 +1,21 @@
+//Variables
+var box = 197;
+let box1 = 452;
+const box2 = 32;
+
+const sumBox = box + box1;
+console.log(sumBox);
+
+//Types
+const interger = 1;
+const nowFloat = 1.5;
+const nowString = "Vainga CHuira Lankaw AGun";
+const nowBoolean = false;
+const weird = NaN;
+
+//Functions
+function addNow(first, second) {
+    const sum = first + second;
+    return sum;
+};
+console.log(addNow(2, 3));
