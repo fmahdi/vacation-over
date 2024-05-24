149c6c1cfce2ca51a8d95fe309aa41a33d9fba01
Fahim Mahdi <fmb.mondal@gmail.com>
2024-05-24 19:44:54 +0600

    aaded js exmaples for loops, functions and conditions

--- scripts/javascript-fundamentals.js
+++ scripts/javascript-fundamentals.js
@@ -19,3 +19,29 @@ function addNow(first, second) {
     return sum;
 };
 console.log(addNow(2, 3));
+
+//arrow fucntion
+const ternDiv = (num1, num2) => num1 / num2;
+console.log(ternDiv(10, 5));
+
+//Conditions
+function multiEven2(number) {
+    if (number % 2 == 0) {
+        const final = number * 2;
+        return final;
+    } else {
+        return "Not an even number";
+    }
+};
+console.log(multiEven2(2));
+console.log(multiEven2(3));
+console.log(multiEven2(0));
+console.log(multiEven2("yes"));
+
+//Looping
+const collection = [12, 13, 14, 15];
+let summUp = 0;
+for (i = 0; i < collection.length; i++) {
+    summUp += collection[i];
+}
+console.log(summUp);
